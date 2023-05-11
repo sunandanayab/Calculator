74f80a61a8d47aa02b12fb390bd123c0d0205695
sunandanayab <104186816+sunandanayab@users.noreply.github.com>
2023-05-11 09:15:31 +0530

    Create MenuOp.java

---
+++ MenuOp.java
@@ -0,0 +1,18 @@
+//MenuOp.java
+package aom;
+public class MenuOp 
+{
+ public static void operations()
+{
+	System.out.println("----------------------------------------");
+    System.out.println("\t\tArithmetic Operations");
+    System.out.println("------------------------------------------");
+    System.out.println("\t\t1. Addition");
+    System.out.println("\t\t2. Subtraction");
+    System.out.println("\t\t3. Multiplication");
+    System.out.println("\t\t4. Division");
+    System.out.println("\t\t5. Modulo");
+    System.out.println("\t\t6. Exit");
+    System.out.println("------------------------------------------");
+}
+}
